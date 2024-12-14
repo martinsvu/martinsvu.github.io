93752a240324be2334666842649ad39ae9683cf7
martinsvu <vuskans.m@gmail.com>
2024-12-14 11:55:08 +0200

    final

--- travelRec/travel_recommendation.js
+++ travelRec/travel_recommendation.js
@@ -7,89 +7,96 @@ const keyword =searchInput.value.toLowerCase();
 const apiUrl ='./travel_recommendation_api.json'
 document.getElementById('search-container').addEventListener('submit',details );
 const timezones =[
-    {name:"Sydney", time:"new Date().toLocaleTimeString('en-US', {timeZone:'Australia/Sydney', hour12: true, hour: 'numeric', minute: 'numeric'});"},
-    {name:"Melbourne", time:"new Date().toLocaleTimeString('en-US', {timeZone:'Australia/Melbourne', hour12: true, hour: 'numeric', minute: 'numeric'}"},
-    {name:"Tokyo", time:"new Date().toLocaleTimeString('en-US', {timeZone:'Asia/Tokyo', hour12: true, hour: 'numeric', minute: 'numeric'}"},
-    {name:"Kyoto", time:"new Date().toLocaleTimeString('en-US', {timeZone:'Asia/Tokyo', hour12: true, hour: 'numeric', minute: 'numeric'}"},
-    {name:"Rio de Janeiro", time:"new Date().toLocaleTimeString('en-US', {timeZone:'America/Sao_Paulo', hour12: true, hour: 'numeric', minute: 'numeric'}"},
-    {name:"São Paulo", time:"Date().toLocaleTimeString('en-US', {timeZone:'America/Sao_Paulo', hour12: true, hour: 'numeric', minute: 'numeric'}"},
-    {name:"Angkor Wat", time:"Date().toLocaleTimeString('en-US', {timeZone:'Asia/Phnom_Penh', hour12: true, hour: 'numeric', minute: 'numeric'}"},
-    {name:"Taj Mahal", time:"Date().toLocaleTimeString('en-US', {timeZone:'Asia/Kolkata', hour12: true, hour: 'numeric', minute: 'numeric'}"},
-    {name:"Bora Bora", time:"Date().toLocaleTimeString('en-US', {timeZone:'Pacific/Tahiti', hour12: true, hour: 'numeric', minute: 'numeric'}"}
+    {name:"Sydney, Australia", timeZone:'Australia/Sydney'},
+    {name:"Melbourne, Australia", timeZone:'Australia/Melbourne'},
+    {name:"Tokyo, Japan", timeZone:'Asia/Tokyo'},
+    {name:"Kyoto, Japan", timeZone:'Asia/Tokyo'},
+    {name:"Rio de Janeiro, Brazil", timeZone:'America/Sao_Paulo'},
+    {name:"São Paulo, Brazil", timeZone:'America/Sao_Paulo'},
+    {name:"Angkor Wat, Cambodia", timeZone:'Asia/Phnom_Penh'},
+    {name:"Taj Mahal, India", timeZone:'Asia/Kolkata'},
+    {name:"Bora Bora, French Polynesia", timeZone:'Pacific/Tahiti'},
+    {name:"Copacabana Beach, Brazil", timeZone:'America/Sao_Paulo'}
 ];
- 
 function details(event) {
     event.preventDefault();
 }
 function clearSearch(){
     searchInput.value="";
     resultDiv.innerHTML="";
-    resultDiv.style.display= "";
+    resultDiv.style.display= "none";
 }
 function search(){
+  const keyword=searchInput.value.toLowerCase();
+  if (keyword ==""){
+    resultDiv.style.display="inherit";
+    resultDiv.innerHTML = "Maybe try typing something in the search box";
+    return;
+  }
     const results = [];
     fetch(apiUrl)
     .then(response => response.json())
         .then(data => {
-            let apidata=[];
-            apidata.push(data);
-            apidata.countries.forEach(country => {
-                // Check the country name
+            data.countries.forEach(country => {
                 if (country.name.toLowerCase().includes(keyword)) {
-                  results.push(country);
-                }
-            
-                // Check the cities of each country
+                  results.push(...country.cities);
+                }else{
                 country.cities.forEach(city => {
-                  if (city.name.toLowerCase().includes(keyword) || city.description.toLowerCase().includes(keyword)) {
+                  if (city.name.toLowerCase().includes(keyword)) {
                     results.push(city);
                   }
-                });
+                });}
               });
-            
-              // Search through temples
-              apidata.temples.forEach(temple => {
-                if (temple.name.toLowerCase().includes(keyword) || temple.description.toLowerCase().includes(keyword)) {
-                  results.push(temple);
-                }
+                  if(keyword.includes("temple")){
+                    results.push(...data.temples);
+                  }
+                data.temples.forEach(temple => {
+                  if (temple.name.toLowerCase().includes(keyword)) {
+                    results.push(temple);
+                  }
               });
-            
-              // Search through beaches
-              apidata.beaches.forEach(beach => {
-                if (beach.name.toLowerCase().includes(keyword) || beach.description.toLowerCase().includes(keyword)) {
-                  results.push(beach);
-                }
+                  if(keyword.includes("beach")){
+                    results.push(...data.beaches);
+                  }else{
+                data.beaches.forEach(beach => {
+                  if (beach.name.toLowerCase().includes(keyword)) {
+                    results.push(beach);
+                  }
+              });}
+                displayResults(results, timezones);
               });
-            
-              // Display the results
-              displayResults(results);
-            }
-            
-            // Function to display the search results
-            function displayResults(results) {
-             
-                resultDiv.innerHTML = ''; // Clear previous results
-            
+}
+            function displayResults(results, timezones) {
+                resultDiv.innerHTML = '';
+                searchInput.value="";
               if (results.length === 0) {
                 resultDiv.innerHTML = 'No results found';
                 return;
               }
-            
               results.forEach(item => {
-                const resultDiv = document.createElement('div');
-                resultDiv.classList.add('result-item');
-                resultDiv.innerHTML = `
+                const timezone = timezones.find(tz => tz.name = item.name);
+                let localTime = 'Time not available';
+                if (timezone) {
+                  localTime = new Date().toLocaleTimeString('en-US', {
+                    timeZone: timezone.timeZone,
+                    hour12: true,
+                    hour: 'numeric',
+                    minute: 'numeric'
+                  });
+                }
+                resultDiv.style.display= "inherit";
+                resultDiv.innerHTML += `
+                  <img src="${item.imageUrl}" alt="${item.name}" width="200">  
                   <h3>${item.name}</h3>
+                  <p>The local time is now:${localTime}<p>
                   <p>${item.description}</p>
-                  <img src="${item.imageUrl}" alt="${item.name}" width="100">
+                  <hr>
                 `;
-                resultDiv.appendChild(resultElement);
               });
             }
-        ;}
 function message(){
     if(contactEmail.value && contactName.value && contactMessage.value !=""){
-        alert("Your message has been recieved!");
+        alert("Your message has been sent!");
         contactEmail.value="";
         contactMessage.value="";
         contactName.value="";
@@ -97,8 +104,3 @@ function message(){
         alert("Please fill out all of the form fields");
     }
 }
-//search term array
-//return object name to var keyword if search term input.value matches any object propertie in search terms array ?find?
-//filter data for keyword return index to index array
-//if data contains keyword return data.foreach.indexarray formated for html from data[indexarray].name, .imgurl . description
-//clear search input
